4349566670af507473c00ea52e3b07163d350b38
Schokomuffin <schokomuffin767@gmail.com>
2026-06-08 20:48:44 +0200

    fix: allow post refresh endpoint

--- backend/api/routes/data.py
+++ backend/api/routes/data.py
@@ -6,5 +6,6 @@ router = APIRouter(prefix="/api/data", tags=["data"])
 
 
 @router.get("/refresh")
+@router.post("/refresh")
 def refresh_data() -> dict[str, str | int]:
     return service.refresh_data()
